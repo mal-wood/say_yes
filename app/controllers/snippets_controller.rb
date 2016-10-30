9449431cd8b55da927d595a03076a4d76b760319
mal-wood <mallorywood413@gmail.com>
2016-10-29 19:56:41 -0700

    Fix create functionality

--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -1,7 +1,31 @@
 class SnippetsController < ActionController::Base
 
 	def index 
-		@snippets = Snippet.all 
+		@user = User.find(current_user.id)
+		@snippets = Snippet.all
 	end 
+
+	def new
+		@user_id = current_user.id
+		@snippet = Snippet.new
+	end
+
+	def show 
+		@snippet = Snippet.find(params[:id])
+	end
 	
+	def create
+		@snippet = Snippet.new(snippet_params)
+		if @snippet.save
+			redirect_to user_snippets_path
+		else
+			@errors = @snippet.errors.full_messages
+		end
+	end
+
+	private 
+	def snippet_params
+		params.require(:snippet).permit(:body, :user_id, :author_name)
+	end
+
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,11 @@ Rails.application.routes.draw do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
 
-  resources :snippets, only:[:index]
-
   resources :users do 
   	resources :snippets
   	resources :friendships
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'snippets#index'
+  root 'users#index'
 end
